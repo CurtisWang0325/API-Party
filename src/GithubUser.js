ccf7cb04c972a9a35489d880bce14d02805260ef
Chien Wang <wangc6@rose-hulman.edu>
2018-06-19 11:05:02 -0400

    add githubuser component and route with params

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -2,9 +2,10 @@ import React , {Component} from 'react'
 
 class GithubUser extends Component{
     render(){
+        const {params} = this.props.match
         return (
             <div className="GithubUser">
-                testing
+                {params.username}
             </div>
         )
     }
